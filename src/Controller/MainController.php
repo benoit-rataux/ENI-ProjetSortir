d29f07f90c2e633ba75c1f7bab5359a94527ce33
Benoît Rataux <benoit.rataux@gmail.com>
2023-01-24 16:08:10 -0500

    ajout de la methode GET sur le path

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', name: 'app_main_')]
 class MainController extends AbstractController {
-    #[Route('', name: 'home')]
+    #[Route('', name: 'home', methods: ['GET'])]
     public function home(): Response {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
