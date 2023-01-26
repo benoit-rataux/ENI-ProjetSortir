11da25c9a00806e902cc070cca485da2d6026ea2
icham7 <118745177+icham7@users.noreply.github.com>
2023-01-26 13:24:07 +0100

    ajout methode creerSortie ajout de la page créer sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SortieRepository;
@@ -16,5 +19,25 @@ class SortieController extends AbstractController
         return $this->render('sortie/sortie.html.twig', [
             "sorties" => $sorties,
         ]);
+    }
+
+    #[Route('/sortie/creer', name: 'app_sortie_creer')]
+    public function creerSortie(Request $request,EntityManagerInterface $entityManager): Response{
+        $sortie = new Sortie();
+
+        $sortieForm = $this->createForm(Sortie::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+
+            $sortie->setOrganisateur($this->getUser());
+            //TODO completer
+
+        }
+
+        return $this->render('sortie/creerSortie.html.twig', [
+            'Sortieform' => $sortieForm->createView()]);
+
+
     }
 }
