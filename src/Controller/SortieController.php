db74c7f88a160e3866dc7b460b6ecaed8101e6c2
Benoît Rataux <benoit.rataux@gmail.com>
2023-01-26 11:19:21 -0500

    [2008] ajout page affichage profil

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
+use App\Form\InscriptionType;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route('/sortie', name: 'app_sortie_')]
 class SortieController extends AbstractController {
@@ -47,10 +49,13 @@ class SortieController extends AbstractController {
         
     }
     
-    #[Route('/detail/{id}', name: 'detail', methods: ['GET'])]
+    #[Route('/detail/{id}', name: 'detail', methods: ['GET', 'POST'])]
     public function detail(
-        int              $id,
-        SortieRepository $sortieRepository,
+        int                    $id,
+        UserInterface          $user,
+        Request                $request,
+        SortieRepository       $sortieRepository,
+        EntityManagerInterface $entityManager,
     ) {
         $sortie = $sortieRepository->find($id);
         
