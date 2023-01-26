ba342af07611ef531df0222878c015850867d2e1
Francois Bridon <43623103+Fbridon@users.noreply.github.com>
2023-01-26 12:01:34 +0100

    modif sortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\SortieRepository;
 
 class SortieController extends AbstractController
 {
     #[Route('/sortie', name: 'app_sortie')]
-    public function index(): Response
+    public function index(SortieRepository $sortieRepository): Response
     {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
+        $sorties = $sortieRepository -> findAll();
+        return $this->render('sortie/sortie.html.twig', [
+            "sorties" => $sorties,
         ]);
     }
 }
