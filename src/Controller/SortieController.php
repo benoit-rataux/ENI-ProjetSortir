4bdc2718b579ab37665295293834d08539af7b49
Benoît Rataux <benoit.rataux@gmail.com>
2023-02-03 12:31:56 +0100

    CORRECTION MERGE Icham

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Campus;
-use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Participant;
 use App\Entity\SearchSortie;
@@ -29,24 +27,24 @@ class SortieController extends AbstractController {
     public function liste(SortieRepository $sortieRepository, UserInterface $user, Request $request): Response {
         /** @var  Participant $user */
         $sorties = $sortieRepository->findAllActiveByCampus($user);
-
+        
         $search     = new SearchSortie();
         $searchForm = $this->createForm(SearchSortieType::class, $search);
         $searchForm->handleRequest($request);
-
+        
         if($searchForm->isSubmitted()) {
             $scriteres = $searchForm->getData();
 //         $sorties = $sortieRepository->findSortiesByName($search->getNomSortie());
 //            $sorties = $sortieRepository->findByCampus($search->getCampus());
             $sorties = $sortieRepository->findByIntervalOfDate($search->getDebutInterval(), $search->getFinInterval());
         }
-
+        
         return $this->render('sortie/listeSortie.html.twig', [
             'sorties'    => $sorties,
             'searchForm' => $searchForm->createView(),
         ]);
     }
-
+    
     #[Route('/creer', name: 'creer', methods: ['GET', 'POST'])]
     public function creerSortie(
         Request            $request,
@@ -54,7 +52,7 @@ class SortieController extends AbstractController {
         SortieEtatsManager $sortieEtatsManager,
     
     ): Response {
-
+        
         /** @var Participant $user */
         $sortie     = new Sortie();
         $sortieForm = $this->createForm(SortieType::class, $sortie);
@@ -76,25 +74,25 @@ class SortieController extends AbstractController {
             'SortieForm' => $sortieForm->createView(), 'sortie' => $sortie,
         ]);
     }
-
-    #[Route('/creerLieu', name:"creerLieu", methods:["GET", "POST"])]
-    public function Lieu(Request $request, EntityManagerInterface $entityManager): Response{
-        $lieu = new Lieu();
+    
+    #[Route('/creerLieu', name: "creerLieu", methods: ["GET", "POST"])]
+    public function Lieu(Request $request, EntityManagerInterface $entityManager): Response {
+        $lieu     = new Lieu();
         $lieuForm = $this->createForm(LieuType::class, $lieu);
         $lieuForm->handleRequest($request);
-
+        
         if($lieuForm->isSubmitted() && $lieuForm->isValid()) {
             $entityManager->persist($lieu);
             $entityManager->flush();
-
+            
             return $this->redirectToRoute('app_sortie_creerLieu');
         }
-
+        
         return $this->render('sortie/creerLieu.html.twig', [
-            'lieuForm' => $lieuForm->createView()
+            'lieuForm' => $lieuForm->createView(),
         ]);
     }
-
+    
     #[Route('/modifier/{id}', name: 'modifier', methods: ['GET', 'POST'])]
     public function modifier(
         Request            $request,
@@ -103,12 +101,12 @@ class SortieController extends AbstractController {
     ) {
         // Controle les droits utilisateurs pour cette action
         $this->denyAccessUnlessGranted(SortieVoter::MODIFIER, $sortie, 'Dinaaaaaayded !!');
-
+        
         $sortieForm = $this->createForm(SortieType::class, $sortie);
         $sortieForm->handleRequest($request);
         $villeForm = $this->createForm(VilleType::class,);
         $villeForm->handleRequest($request);
-
+        
         if($sortieForm->isSubmitted() && $sortieForm->isValid()) {
             try {
                 $sortieEtatsManager->modifier($sortie);
@@ -118,8 +116,12 @@ class SortieController extends AbstractController {
             }
             return $this->redirectToRoute('app_sortie_liste');
         }
+        
+        return $this->render('sortie/modifierSortie.html.twig', [
+            'SortieForm' => $sortieForm->createView(), 'sortie' => $sortie,
+        ]);
     }
-
+    
     #[Route('/supprimer/{id}', name: 'supprimer', methods: ['GET'])]
     public function supprimer(
         Sortie             $sortie,
@@ -127,14 +129,14 @@ class SortieController extends AbstractController {
     ) {
         // Controle les droits utilisateurs pour cette action
         $this->denyAccessUnlessGranted(SortieVoter::SUPPRIMER, $sortie, 'Dinaaaaaayded !!');
-
+        
         try {
             $sortieEtatsManager->supprimer($sortie);
             $this->addFlash('success', 'Votre sortie "' . $sortie->getNom() . '" a bien été supprimée');
         } catch(BLLException $e) {
             $this->addFlash('error', $e->getMessage());
         }
-
+        
         return $this->redirectToRoute('app_main_home');
     }
     
